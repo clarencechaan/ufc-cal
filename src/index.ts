be99d4f5bd0ed8990ca84fb24525e3afae00f691
Clarence Chan <clarencechaan@icloud.com>
2025-11-16 13:06:43 -0500

    Fix #2

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ async function createICS() {
     if (eventsData) fs.writeFileSync("UFC.ics", eventsData);
 
     // Filter for PPV events only
-    const ppvEvents = events.filter((event) => /^UFC \d+/.test(event.name));
+    const ppvEvents = events.filter((event) => /UFC \d+/.test(event.name));
     const formattedPPVEvents = ppvEvents.map((event) =>
       formatEventForCalendar(event, "UFC-PPV")
     );
